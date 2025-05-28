3607c98d518facb183f8a5d6ec7c111360f43b5e
melomatthewdev <devscustomers@outlook.com>
2025-05-28 16:12:31 -0500

    actualizacion de archivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import { div } from "three/examples/jsm/nodes/Nodes.js"
 
-import * as Sentry from '@sentry/react';
 
 const App = () => {
   return (
-    <main className="bg-black">
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
-    </main>
+    <div className="bg-gray-800 text-white">
+      Hello word
+    </div>
+
   )
 }
 
-export default Sentry.withProfiler(App);
+export default App
